b72dbfd726f178e10ab578191e57464ad3d5c3b0
catarinasapateiro <catarinasapateiro@hotmail.com>
2025-06-04 16:41:35 +0100

    multichoice-question

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ export { default as Footer } from "./Footer";
 export { default as WelcomeScreen } from "./WelcomeScreen";
 export { default as Continent } from "./Continent";
 export { default as Categories } from "./Categories";
+export { default as MultiChoice } from "./MultiChoice";
 
 export * from "./ui";
