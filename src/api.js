b380fca506cd83e13f4f0235640f5af48c651811
naydenhristov <nhristov21@gmail.com>
2025-06-05 14:24:32 +0100

    16 - API calls partially done

---
+++ src/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const geoExplorerApi = axios.create({
+    baseURL: "https://geo-explorer-be.onrender.com/api"
+});
+
+export const getSubCategories = () => {
+    return geoExplorerApi.get("/subcategories").then(({data}) => {
+       return(data.subcategories);
+    });
+};
+
+export const getUsers = () => {
+    return geoExplorerApi.get("/users").then(({data}) => {
+       return(data.users);
+    });
+};
+
+export const getUserByUserId = (username) => {
+    return geoExplorerApi.get(`/users/${username}`).then(({data}) => {
+       return(data.user);
+    });
+};
+
+export const getLeasrningCards = () => {
+    return geoExplorerApi.get("/learning-cards").then(({data}) => {
+       return(data.learningCards);
+    });
+};
+
+export const getMatchingPairs = () => {
+    return geoExplorerApi.get("/matching-pairs").then(({data}) => {
+       return(data.matchingPairs);
+    });
+};
+
+export const getMultichoiceQAs = () => {
+    return geoExplorerApi.get("/multichoice_qa").then(({data}) => {
+       return(data.multichoice_qa);
+    });
+};
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,10 +1,29 @@
 "use client";
 
-const { createContext, useState } = require("react");
+import { getUsers } from "@/api";
+
+const { createContext, useState, useEffect } = require("react");
 
 export const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
+  const [usersList, setUsersList] = useState([]);
+  const [user, setUser] = useState("");
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getUsers()
+      .then((users) => {
+        setUsersList(users);
+      })
+      .catch((err) => {
+        setError(true);
+      });
+  });
+
+  if (error) return <p>Sorry, something went wrong!</p>;
+  console.log(usersList, "<---userlist");
+  
   //test need to change later
   // {
   //   user_id: 1,
@@ -14,7 +33,6 @@ export const UserContextProvider = ({ children }) => {
   //   rating: 0,
   //   avatar_url: "https://avatar.iran.liara.run/public/35",
   // }
-  const [user, setUser] = useState();
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
