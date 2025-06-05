689770de4a4ef53c2f61e0b17250fb253c62f16c
Wai Yan Sze <szewaiyan@Wais-MacBook-Air.local>
2025-06-05 20:27:52 +0100

    css changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { LogoIcon, UserIcon } from "@/assets";
 
 const Header = () => {
   return (
-    <header className="container mx-auto px-4 lg:max-w-5xl flex justify-between items-center p-4 shadow-md rounded-sm">
+    <header className="container sticky top-0 z-1 bg-white mx-auto px-4 lg:max-w-5xl flex justify-between items-center p-4 shadow-md rounded-sm">
       <Link href="/">
         <LogoIcon />
       </Link>
