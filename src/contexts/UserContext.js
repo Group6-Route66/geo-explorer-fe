78ea4036db19964f644d02203a10548f0474e002
Phone Sett Paing <mr.phonesettpaing@gmail.com>
2025-06-05 15:22:49 +0100

    User Profile Redesign

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,15 +5,7 @@ const { createContext, useState } = require("react");
 export const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
-  //test need to change later
-  // {
-  //   user_id: 1,
-  //   username: "john_s",
-  //   level_nature: "beginner",
-  //   level_territory: "beginner",
-  //   rating: 0,
-  //   avatar_url: "https://avatar.iran.liara.run/public/35",
-  // }
+
   const [user, setUser] = useState();
 
   return (
