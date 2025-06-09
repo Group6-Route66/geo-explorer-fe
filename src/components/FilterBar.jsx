67ef0d4844d6e8a14cd8e200bc5e0ef7dd96d0f8
Wai Yan Sze <szewaiyan@Wais-MacBook-Air.local>
2025-06-09 16:53:00 +0100

    Fixed the problem of Each child in a list should have a unique key prop.

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -71,9 +71,9 @@ export default function FilterBar({
         {/* Category button group */}
         {showCategories && (
           <div className="w-full flex justify-between items-center rounded-md ">
-            {categories.map(({ id, category_name }) => (
+            {categories.map(({ category_id, category_name }) => (
               <button
-                key={id}
+                key={category_id}
                 onClick={() => setActiveCategory(category_name)}
                 className={`pb-2 border-b-4 mr-1 ml-1 ${
                   activeCategory === category_name
