8633fb71f3ad60a5330cb44ffac74145921a69b7
Wai Yan Sze <szewaiyan@Wais-MacBook-Air.local>
2025-06-09 16:57:58 +0100

    fixed the css

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -98,7 +98,7 @@ export default function FilterBar({
                 className={`pb-2 border-b-4 mr-1 ml-1 ${
                   subCategoryId === id
                     ? "border-[var(--color-green)] w-100 font-bold text-[var(--color-green)]"
-                    : "border-[#51545d] w-100 text--[var(--color-gray-900)] hover:text-[var(--color-green)]"
+                    : "border-[var-(color-gray-900)] w-100 text--[var(--color-gray-900)] hover:text-[var(--color-green)]"
                 }`}
                 type="button"
               >
